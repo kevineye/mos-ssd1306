34cdea480d5766e2e41528435c15575eef7f3474
Kevin Eye <eye@buffalo.edu>
2017-11-27 06:50:21 -0500

    switched from arduino native lib to mgos

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,12 @@
 #include "mgos.h"
 
-#include <Adafruit_SSD1306.h>
+#include <mgos_arduino_ssd1306.h>
 
-Adafruit_SSD1306 display(16, Adafruit_SSD1306::RES_128_64);
-
-static void led_timer_cb(void *arg) {
-  bool val = mgos_gpio_toggle(2);
-  LOG(LL_INFO, ("%s uptime: %.2lf, RAM: %lu, %lu free", val ? "Tick" : "Tock",
-                mgos_uptime(), (unsigned long) mgos_get_heap_size(),
-                (unsigned long) mgos_get_free_heap_size()));
-  (void) arg;
-}
+Adafruit_SSD1306* display = mgos_ssd1306_create_i2c(16, Adafruit_SSD1306::RES_128_64);
 
 enum mgos_app_init_result mgos_app_init(void) {
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  display.display();
+  mgos_ssd1306_begin(display, SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS, true);
+  mgos_ssd1306_display(display);
 
-  mgos_gpio_set_mode(2, MGOS_GPIO_MODE_OUTPUT);
-  mgos_set_timer(1000, MGOS_TIMER_REPEAT, led_timer_cb, NULL);
-  
   return MGOS_APP_INIT_SUCCESS;
 }
