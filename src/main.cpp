b3fd51e10a2934a0ce86146bd4c4f2d50ad42fc3
Kevin Eye <eye@buffalo.edu>
2017-11-27 08:24:26 -0500

    moved ssd1306 reset pin to config

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "mgos.h"
+#include "mgos_arduino_ssd1306.h"
 
-#include <mgos_arduino_ssd1306.h>
-
-Adafruit_SSD1306* display = mgos_ssd1306_create_i2c(16, Adafruit_SSD1306::RES_128_64);
+Adafruit_SSD1306* display;
 
 enum mgos_app_init_result mgos_app_init(void) {
+  display = mgos_ssd1306_create_i2c(mgos_sys_config_get_app_ssd1306_reset_pin(), Adafruit_SSD1306::RES_128_64);
   mgos_ssd1306_begin(display, SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS, true);
   mgos_ssd1306_display(display);
 
